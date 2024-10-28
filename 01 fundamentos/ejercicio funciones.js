457d8ca91e98def2625e94d77bc16d5f4f33f612
Blackbear26 <163040073+Blackbear26@users.noreply.github.com>
2024-10-28 17:04:14 -0600

    practica de functions

---
+++ 01 fundamentos/ejercicio funciones.js
@@ -0,0 +1,14 @@
+let heroe = prompt("elige a un heroe")
+
+function obtenerPoder (heroe) {
+  if (heroe === superman) {
+  console.log("tu poder es superfuerza")}
+  else if (heroe === flash){
+  console.log("tu poder es super velocidad")}
+  else if (heroe === bataman){
+  console.log("tu poder es super millonario")}
+  else ("no te initaron a la fiesta")
+heroe[0] = "superheroe";
+}
+
+obtenerPoder(heroe)
